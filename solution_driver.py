508cacf18df1fa01f0672c8d9cd8e9b6d83c2409
Zack <zhaozeyu@seas.upenn.edu>
2018-08-12 21:13:33 -0400

    int to roman

--- solution_driver.py
+++ solution_driver.py
@@ -1,5 +1,4 @@
 if __name__ == '__main__':
-    from containerMostWater import Solution
-    height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+    from intToRoman import Solution
     sol = Solution()
-    print(sol.maxArea(height))
+    print(sol.intToRoman(1994))
