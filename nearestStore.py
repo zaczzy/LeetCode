1ff0cbf45a761e14c92ece4cf1daf3190bb5fe90
zaczzy <zhaozeyu@seas.upenn.edu>
2018-10-01 22:00:16 -0400

    pre-pull

---
+++ nearestStore.py
@@ -0,0 +1,10 @@
+# the inputs are two arrays of 1d location of stores and houses
+# return an array of 1d location of closest stores for each house.
+
+
+def nearestStore(stores, houses):
+    nearest = {}
+    current = None  # the closest store with smaller location
+    i = 0
+    j = 0
+    return
